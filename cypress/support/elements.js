58847fd28f6e53354cf164939a777f752ef67ef7
Jussara Rosa <126550432+JussaraRosaQA@users.noreply.github.com>
2023-08-21 12:41:34 -0300

    Update elements.js
    Refinamento

--- cypress/support/elements.js
+++ cypress/support/elements.js
@@ -1,30 +1,14 @@
-//com seletor
 export const Form = {
       INPUT: {
             ACCEPT: '.EiWNMp',
             CAMP: 'h2 > span > span',
-            NAME: '#input_comp-kwz6tqej',
-            EMAIL: '#input_comp-kwz6tqf7',
-            TELEPHONE: '#input_comp-kwz6tqfe',
-            MESSAGE: '#textarea_comp-kwz6tqfr',
-            CHECK: '#comp-kwz6tqgp > input',
+            NAME: 'input[name="nome"]',
+            EMAIL: 'input[name="email"]',
+            TELEPHONE: 'input[name="phone"]',
+            MESSAGE: '[placeholder="Nos conte o seu desafio"]',
+            CHECK: 'input[data-testid="input"]',
       },
       BUTTON: {
-            SUBMIT: '#comp-kwz6tqhd > button',
+            SUBMIT: 'button[data-testid="buttonElement"]',
       },
 }
-//em X-Path
-// export const Form = {
-//       INPUT: {
-//             ACCEPT: '.EiWNMp',
-//             CAMP: 'h2 > span > span',
-//             NAME: '/html/body/div/div/div[3]/div/main/div/div/div/div[2]/div/div/div/div[3]/div/div/form/div/div/div[1]/div/input',
-//             EMAIL: '/html/body/div/div/div[3]/div/main/div/div/div/div[2]/div/div/div/div[3]/div/div/form/div/div/div[2]/div/input',
-//             TELEPHONE: '/html/body/div/div/div[3]/div/main/div/div/div/div[2]/div/div/div/div[3]/div/div/form/div/div/div[3]/div/input',
-//             MESSAGE: '/html/body/div/div/div[3]/div/main/div/div/div/div[2]/div/div/div/div[3]/div/div/form/div/div/div[4]/textarea',
-//             CHECK: '/html/body/div/div/div[3]/div/main/div/div/div/div[2]/div/div/div/div[3]/div/div/form/div/div/label/input',
-//       },
-//       BUTTON: {
-//             SUBMIT: '/html/body/div/div/div[3]/div/main/div/div/div/div[2]/div/div/div/div[3]/div/div/form/div/div/div[5]/button',
-//       },
-// }
